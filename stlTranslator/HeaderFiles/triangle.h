333087404cf2e22c41863ed5017e45b8b92fd99a
AryanCCTech <aryan.yadav@cctech.co.in>
2024-10-24 11:10:58 +0530

    Update triangle.h

--- stlTranslator/HeaderFiles/triangle.h
+++ stlTranslator/HeaderFiles/triangle.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "point.h"
 
-class Triangle {
-    Point p1, p2, p3;
-
+class Triangle 
+{
+    Point p1;
+    Point p2;
+    Point p3;
 public:
     Triangle(Point p1, Point p2, Point p3);
     Point getPointP1();
