922e012266a798e2b715156c89f6c1789edda14d
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-24 11:55:22 +0530

    Update and rename triangulation.h to Triangulation.h

--- stlTranslator/HeaderFiles/Triangulation.h
+++ stlTranslator/HeaderFiles/Triangulation.h
@@ -5,11 +5,18 @@
 
 class Triangulation 
 {
+// Vector to store Triangle objects that form the triangulation.
     std::vector<Triangle> triangles;
+// Vector to store unique vertex points.
     std::vector<double> uniqueVertices;
 public: 
+
+    //Adds a unique vertex to the uniqueVertices vector
     void setUniqueVertices(double value);
+    //Adds a Triangle object to the triangles vector.
     void setDataStructure(Triangle triangle);
+     // Return the vector of unique vertex points
     std::vector<double> getUniqueVertices();
+    // // Return the vector of Triangle objects
     std::vector<Triangle> triangulizationDataStructure();
 };
