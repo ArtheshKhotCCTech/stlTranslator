fd6e25a386287e04d5b004abc4413edb995804cd
AryanCCTech <aryan.yadav@cctech.co.in>
2024-10-24 11:22:08 +0530

    Update DATWriter.cpp

--- stlTranslator/SourceFiles/DATWriter.cpp
+++ stlTranslator/SourceFiles/DATWriter.cpp
@@ -3,12 +3,14 @@
 void DATWriter::fileWriter(const std::string& filename, const std::vector<Triangle>& triangles, const std::vector<double>& uniqueVertices)
 {
     std::ofstream outfile(filename);
-    for (Triangle triangle : triangles) {
+    for (Triangle triangle : triangles) 
+    {
         outfile << std::fixed << std::setprecision(6)
             << uniqueVertices[triangle.getPointP1().getX()] << " " << uniqueVertices[triangle.getPointP1().getY()] << " " << uniqueVertices[triangle.getPointP1().getZ()] << "\n"
             << uniqueVertices[triangle.getPointP2().getX()] << " " << uniqueVertices[triangle.getPointP2().getY()] << " " << uniqueVertices[triangle.getPointP2().getZ()] << "\n"
             << uniqueVertices[triangle.getPointP3().getX()] << " " << uniqueVertices[triangle.getPointP3().getY()] << " " << uniqueVertices[triangle.getPointP3().getZ()] << "\n"
-            << uniqueVertices[triangle.getPointP1().getX()] << " " << uniqueVertices[triangle.getPointP1().getY()] << " " << uniqueVertices[triangle.getPointP1().getZ()] << "\n" <<std::endl<<std::endl;
+            << uniqueVertices[triangle.getPointP1().getX()] << " " << uniqueVertices[triangle.getPointP1().getY()] << " " << uniqueVertices[triangle.getPointP1().getZ()] << "\n" 
+            << std::endl << std::endl;    //2 linebreaks as per GNUPlot format.
     }
     std::cout << "Data file created !!!!" << std::endl;
 }
